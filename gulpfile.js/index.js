493997db10916645a7e46caf3fe6394ecac8f32f
Nils Persson <nils@nedge.se>
2019-09-04 15:33:12 +0200

    Add JS gulp task

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,13 +1,25 @@
 const { series, src, dest } = require('gulp');
+const babel = require('gulp-babel');
 const ftpCredentials = require('./ftp-credentials');
 
 const PATH = Object.freeze({
   DEST: 'wp-content',
-  PHP: 'src/**/*.php'
+  PHP: 'src/**/*.php',
+  JS: 'src/**/*.js',
+  CSS: 'src/**/*.css',
+  IMG: ['src/**/*.png', 'src/**/*.jpg', 'src/**/*.jpeg']
 })
 
 function php() {
   return src(PATH.PHP)
+    .pipe(dest(PATH.DEST));
+}
+
+function javascript() {
+  return src(PATH.JS)
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
     .pipe(dest(PATH.DEST));
 }
 
@@ -17,4 +29,5 @@ function defaultTask(cb) {
   cb();
 }
 
+exports.javascript = javascript;
 exports.default = defaultTask;
