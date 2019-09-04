9e99f63329acba7d2fdff370ecef711d336c5102
Nils Persson <nils@nedge.se>
2019-09-04 15:20:16 +0200

    Add PHP build task

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,4 +1,15 @@
+const { series, src, dest } = require('gulp');
 const ftpCredentials = require('./ftp-credentials');
+
+const PATH = Object.freeze({
+  DEST: 'wp-content',
+  PHP: 'src/**/*.php'
+})
+
+function php() {
+  return src(PATH.PHP)
+    .pipe(dest(PATH.DEST));
+}
 
 function defaultTask(cb) {
   // place code for your default task here
