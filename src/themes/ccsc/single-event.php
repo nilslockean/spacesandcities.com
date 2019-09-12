32938102e4fba062324721be293a3f9de400e636
Nils Persson <nils@nedge.se>
2019-09-12 15:31:04 +0200

    Extract custom sidebar to function

--- src/themes/ccsc/single-event.php
+++ src/themes/ccsc/single-event.php
@@ -16,19 +16,7 @@
 			?>
 		</div>
 
-		<!-- <?php FLTheme::sidebar( 'right' ); ?> -->
-    <?php
-      $parent_sidebar = 'blog';
-      $section = 'single-event';
-      $position = 'right';
-      $size    = FLTheme::get_setting( 'fl-' . $parent_sidebar . '-sidebar-size' );
-      $display = FLTheme::get_setting( 'fl-' . $parent_sidebar . '-sidebar-display' );
-      $layout  = FLTheme::get_setting( 'fl-' . $parent_sidebar . '-layout' );
-
-      if ( strstr( $layout, $position ) && FLTheme::is_sidebar_enabled( $parent_sidebar ) ) {
-        include locate_template( 'sidebar.php' );
-      }
-    ?>
+    <?php ccsc_sidebar('single-event'); ?>
 
 	</div>
 </div>
