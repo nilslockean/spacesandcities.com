226ccc93a5293e404c06b36b590aabb214ee4a13
Nils Persson <nils@nedge.se>
2019-09-12 15:30:11 +0200

    Register Urban Lab sidebar

--- src/themes/ccsc/functions.php
+++ src/themes/ccsc/functions.php
@@ -22,6 +22,14 @@ if ( function_exists('register_sidebar') ) {
     'after_title' => '</h2>'
    ));
 
+   register_sidebar(array(
+    'name' => 'Single Urban Lab',
+    'id' => 'single-urban-lab-sidebar',
+    'before_widget' => '<div id="%1$s" class="widget %2$s">',
+    'after_widget' => '</div>',
+    'before_title' => '<h2>',
+    'after_title' => '</h2>'
+   ));
 }
 
 // Urban Labs Post Type
