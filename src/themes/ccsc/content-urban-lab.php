5fa5c8af15368db96eed89a889b5fabdb7c813f6
Nils Persson <nils@nedge.se>
2019-09-18 16:19:55 +0200

    Update Urban Lab content template

--- src/themes/ccsc/content-urban-lab.php
+++ src/themes/ccsc/content-urban-lab.php
@@ -24,29 +24,8 @@ $thumb_size  = FLTheme::get_setting( 'fl-posts-thumb-size' );
 			<?php the_title(); ?>
 			<?php edit_post_link( _x( 'Edit', 'Edit post link text.', 'fl-automator' ) ); ?>
 		</h1>
-		<?php FLTheme::post_top_meta(); ?>
 	</header><!-- .fl-post-header -->
-
-	<?php if ( has_post_thumbnail() && ! empty( $show_thumbs ) ) : ?>
-		<?php if ( 'above' == $show_thumbs ) : ?>
-		<div class="fl-post-thumb">
-			<?php the_post_thumbnail( 'large' ); ?>
-		</div>
-		<?php endif; ?>
-
-		<?php if ( 'beside' == $show_thumbs ) : ?>
-			<div class="row fl-post-image-<?php echo $show_thumbs; ?>-wrap">
-				<div class="fl-post-image-<?php echo $show_thumbs; ?>">
-					<div class="fl-post-thumb">
-						<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
-							<?php the_post_thumbnail( $thumb_size ); ?>
-						</a>
-					</div>
-				</div>
-				<div class="fl-post-content-<?php echo $show_thumbs; ?>">
-		<?php endif; ?>
-	<?php endif; ?>
-
+	
 	<?php do_action( 'fl_before_post_content' ); ?>
 
 	<div class="fl-post-content clearfix" itemprop="text">
